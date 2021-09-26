a77f0a0c1650fa13616496d009b3df82dae3996f
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2021-09-26 10:47:33 +0530

    Update resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import resume from "../assets/resume.pdf";
+import resume from "../assets/ShreyanshShrey_Resume1.pdf";
 import picture from "../images/thumbs/profilepic.jpg";
 
 class About extends Component {
